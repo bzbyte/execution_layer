ceab68c16fd4b0dd898ed6041c266f1e97f3ceb4
faraz <faraz.shaikh@gmail.com>
2023-03-23 13:56:19 -0700

    Remove dep from arbitrary

--- tests/driver.rs
+++ tests/driver.rs
@@ -1,4 +1,4 @@
-use execution_layer::engine_api::{
+use bzb_execution_layer::engine_api::{
     auth::{Auth, JwtKey},
     ethspec::MainnetEthSpec,
     execution_payload::Hash256,
@@ -21,53 +21,53 @@ fn driver() {
         let capabilities = rpc_client.exchange_capabilities().await.unwrap();
         println!("Caps: {capabilities:?}");
 
-        loop {
-            rpc_client.upcheck().await.unwrap();
-            let block = rpc_client
-                .get_block_by_number(BlockByNumberQuery::Tag(LATEST_TAG))
-                .await
-                .unwrap()
-                .unwrap();
-            //println!("Latest Block: {block:?}");
-            let f = ForkchoiceState {
-                head_block_hash: block.block_hash,
-                safe_block_hash: block.block_hash,
-                finalized_block_hash: ExecutionBlockHash::zero(),
-            };
-            let attr = Some(PayloadAttributes::V1(PayloadAttributesV1 {
-                timestamp: block.timestamp + 1,
-                prev_randao: Hash256::zero(),
-                suggested_fee_recipient: Address::repeat_byte(0),
-            }));
-            let fchoice_result = rpc_client.forkchoice_updated_v2(f, attr).await.unwrap();
-            //println!("Fork choice {fchoice_result:?}");
+        //        loop {
+        rpc_client.upcheck().await.unwrap();
+        let block = rpc_client
+            .get_block_by_number(BlockByNumberQuery::Tag(LATEST_TAG))
+            .await
+            .unwrap()
+            .unwrap();
+        println!("Latest Block: {block:?}");
+        let f = ForkchoiceState {
+            head_block_hash: block.block_hash,
+            safe_block_hash: block.block_hash,
+            finalized_block_hash: ExecutionBlockHash::zero(),
+        };
+        let attr = Some(PayloadAttributes::V1(PayloadAttributesV1 {
+            timestamp: block.timestamp + 1,
+            prev_randao: Hash256::zero(),
+            suggested_fee_recipient: Address::repeat_byte(0),
+        }));
+        let fchoice_result = rpc_client.forkchoice_updated_v2(f, attr).await.unwrap();
+        println!("Fork choice {fchoice_result:?}");
 
-            let payload = rpc_client
-                .get_json_payload_v1::<MainnetEthSpec>(fchoice_result.payload_id.unwrap())
-                .await
-                .unwrap()
-                .into();
+        let payload = rpc_client
+            .get_json_payload_v1::<MainnetEthSpec>(fchoice_result.payload_id.unwrap())
+            .await
+            .unwrap()
+            .into();
 
-            //println!("New payload {payload:?}");
-            let payload_result = rpc_client.new_payload_v1(payload).await.unwrap();
-            println!("New payload result {payload_result:?}");
+        println!("New payload {payload:?}");
+        let payload_result = rpc_client.new_payload_v1(payload).await.unwrap();
+        println!("New payload result {payload_result:?}");
 
-            // next state
-            let next_fork_choice = ForkchoiceState {
-                head_block_hash: payload_result.latest_valid_hash.unwrap(),
-                safe_block_hash: payload_result.latest_valid_hash.unwrap(),
-                finalized_block_hash: ExecutionBlockHash::zero(),
-            };
-            let attr = Some(PayloadAttributes::V1(PayloadAttributesV1 {
-                timestamp: block.timestamp + 2,
-                prev_randao: Hash256::zero(),
-                suggested_fee_recipient: Address::repeat_byte(0),
-            }));
-            let _fchoice_result = rpc_client
-                .forkchoice_updated_v2(next_fork_choice, attr)
-                .await
-                .unwrap();
-        }
+        // next state
+        let next_fork_choice = ForkchoiceState {
+            head_block_hash: payload_result.latest_valid_hash.unwrap(),
+            safe_block_hash: payload_result.latest_valid_hash.unwrap(),
+            finalized_block_hash: ExecutionBlockHash::zero(),
+        };
+        let attr = Some(PayloadAttributes::V1(PayloadAttributesV1 {
+            timestamp: block.timestamp + 2,
+            prev_randao: Hash256::zero(),
+            suggested_fee_recipient: Address::repeat_byte(0),
+        }));
+        let _fchoice_result = rpc_client
+            .forkchoice_updated_v2(next_fork_choice, attr)
+            .await
+            .unwrap();
+        //      }
     });
 }
 
